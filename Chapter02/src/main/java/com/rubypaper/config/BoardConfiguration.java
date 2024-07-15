770d16814ec16e6142875581ea559e9642931d20
flowerdeerjeannie <flowerdeer121212@gmail.com>
2024-07-15 12:59:29 +0900

    jdbc connection 연습

---
+++ Chapter02/src/main/java/com/rubypaper/config/BoardConfiguration.java
@@ -0,0 +1,20 @@
+package com.rubypaper.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.rubypaper.jdbc.util.JDBCConnectionManager;
+
+@Configuration
+public class BoardConfiguration {
+
+	@Bean
+	JDBCConnectionManager getJDBCConectionManager() {
+		JDBCConnectionManager manager = new JDBCConnectionManager();
+		manager.setUrl("jdbc:mysql://localhost:3306/musthave");
+		manager.setUsername("scott");
+		manager.setPassword("tiger");
+		return manager;
+	}
+}
